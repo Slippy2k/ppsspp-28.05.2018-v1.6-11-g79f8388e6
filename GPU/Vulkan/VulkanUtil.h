564b739e77139afff63c9152c959056384f2b7b6
Henrik Rydgård <hrydgard@gmail.com>
2017-12-11 14:48:19 +0100

    Fix build error on 64-bit android

--- GPU/Vulkan/VulkanUtil.h
+++ GPU/Vulkan/VulkanUtil.h
@@ -164,7 +164,7 @@ private:
 		VkShaderModule module;
 	};
 	
-	DenseHashMap<PipelineKey, VkPipeline, VK_NULL_HANDLE> pipelines_;
+	DenseHashMap<PipelineKey, VkPipeline, (VkPipeline)VK_NULL_HANDLE> pipelines_;
 };
 
 
